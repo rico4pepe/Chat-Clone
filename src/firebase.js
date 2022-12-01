a04afcec0ed554a1b5ffa9be9243804f4ca880ea
rico4pepe <rico4pepe@gmail.com>
2022-12-01 12:46:28 +0100

    Chat clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCS95ZgsgkI9Ytc-GFm5aD0Kat1ezWnzUA",
+    authDomain: "whatsapp-clone-e0f92.firebaseapp.com",
+    projectId: "whatsapp-clone-e0f92",
+    storageBucket: "whatsapp-clone-e0f92.appspot.com",
+    messagingSenderId: "1090776557667",
+    appId: "1:1090776557667:web:730d666a1c250993bdf056",
+    measurementId: "G-D4EMMZG96T"
+  };
