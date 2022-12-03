8561c0feee1c2dd7ea49ce2d1ede4dcb150d916f
rico4pepe <rico4pepe@gmail.com>
2022-12-03 17:23:34 +0100

    Add pusher functionalities to the front end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import ChatComponent from "./compoents/ChatComponent";
 import Sidebar from "./compoents/Sidebar";
 import './App.css';
+import {useEffect} from 'react'
 
 
 function App() {
+  useEffect(() => {
+    // Run  this piece of code once the app load
+    const pusher = new Pusher('51874f4b2abc85062490', {
+      cluster: 'mt1'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+  }, [])
   return (
     <div className="app">
       <div className="app__body">
